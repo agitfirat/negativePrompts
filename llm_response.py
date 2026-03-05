d750e51f7486037139f6d53719e19197c42cd9c1
gillesganesh <gillesganesharajah@gmail.com>
2026-03-05 10:36:59 +0100

    fix: auto device selection CPU/GPU, remove broken BLOOM block

--- llm_response.py
+++ llm_response.py
@@ -1,6 +1,7 @@
 import time
 import re
 
+
 def get_match_items(items, str):
     match_time = 0
     str = str.lower()
@@ -21,8 +22,6 @@ def locate_ans(query, output):
     input_line = input_line.strip()
     inputs = input_line.split()
     output_lines = output.split('\n')
-    # print(output_lines)
-    # print('IN: ', inputs)
     ans_line = ''
     max_match_time = 0
     for i in range(len(output_lines)):
@@ -35,72 +34,28 @@ def locate_ans(query, output):
                 ans_line += output_lines[i+1]
             if i < len(output_lines) - 2:
                 ans_line += output_lines[i+2]
-    # print('ANSLine: ', ans_line)
-    # ans = ''
-    # if len(ans_line) > 0:
-    #     last_index = 0
-    #     for i in inputs:
-    #         i = i.strip()
-    #         i_index = ans_line.rfind(i)
-    #         if i_index > last_index:
-    #             last_index = i_index
-    #             ans = i
-    # print('ANS: ', ans)
     return ans_line
-    
+
+
 api_num = 5
+
 
 def get_response_from_llm(llm_model, queries, task, few_shot, api_num=4):
     model_outputs = []
-        import requests
-        API_URL = "https://api-inference.huggingface.co/models/bigscience/bloom"
-        headers = {"Authorization": ""}
-        def query(payload):
-            response = requests.post(API_URL, headers=headers, json=payload)
-            return response.json()
-        for q in queries:
-            output = None
-            while output == None or len(output) <= 0:
-                output = query({
-                    "inputs": q,
-                })
-            output = output[0]['generated_text']
-            print('Q: ', q)
-            print('OUTPUT: ', output)
-            out_list = re.findall("Answer:.*", output)
-            explain_list = re.findall("Explanation:.*", output)
-            # print(out_list)
-            output = ''
-            if len(out_list) > 0:
-                output = out_list[0].replace('Answer:', '')
-                output = output.strip()
-            if len(explain_list) > 0:
-                explain = explain_list[0].replace('Explanation:', ' ')
-                explain = explain.strip()
-                if task == 'larger_animal' or task == 'word_in_context':
-                    output += explain
-            model_outputs.append(output)
+
     if llm_model.lower() == 't5':
+        import torch
         from transformers import T5Tokenizer, T5ForConditionalGeneration
-        # gpu-version
-        # device = torch.device('cuda:0')
-        tokenizer = T5Tokenizer.from_pretrained("google/flan-t5-large")
-        model = T5ForConditionalGeneration.from_pretrained("google/flan-t5-large", device_map="auto")
-        # print(model.device)
 
-        # cpu-version
-        # tokenizer = T5Tokenizer.from_pretrained("google/flan-t5-large")
-        # model = T5ForConditionalGeneration.from_pretrained("google/flan-t5-large")
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+        print(f"Using device: {device}")
+
+        tokenizer = T5Tokenizer.from_pretrained("google/flan-t5-large")
+        model = T5ForConditionalGeneration.from_pretrained("google/flan-t5-large").to(device)
 
         for q in queries:
-            # cpu
-            # input_ids = tokenizer(q, return_tensors="pt").input_ids
-            # outputs = model.generate(input_ids)
-            
-            # gpu
-            input_ids = tokenizer(q, return_tensors="pt").input_ids.to("cuda")
+            input_ids = tokenizer(q, return_tensors="pt").input_ids.to(device)
             outputs = model.generate(input_ids)
-            
             out_text = tokenizer.decode(outputs[0])
             out_text = out_text.replace('<pad>', '')
             out_text = out_text.replace('</s>', '')
@@ -108,40 +63,26 @@ def get_response_from_llm(llm_model, queries, task, few_shot, api_num=4):
             out_text = out_text.strip()
             print('Model Output: ', out_text)
             model_outputs.append(out_text)
+
     elif llm_model.lower() == 'vicuna':
         import openai
-        openai.api_key = "EMPTY" # Not support yet
+        openai.api_key = "EMPTY"
         openai.api_base = "http://0.0.0.0:8000/v1"
 
         model = "vicuna-13b-v1.1"
-        # prompt = "Hello! What is your name?"
+
         def get_completion(prompt):
             response = openai.ChatCompletion.create(
                 model=model,
                 messages=[{"role": "user", "content": prompt}],
                 max_tokens=200)
-            # response = openai.Completion.create(model=model, prompt=prompt, max_tokens=64)
             return response.choices[0].message.content
 
-        # from transformers import LlamaForCausalLM, LlamaTokenizer, AutoTokenizer, AutoModelForCausalLM
-        # import torch
-
-        # model_dir = '/root/autodl-tmp/model/vicuna-13b-v1.5'
-        #
-        # tokenizer = AutoTokenizer.from_pretrained(model_dir, device_map="auto", use_fast=False)
-        # pipe = AutoModelForCausalLM.from_pretrained(model_dir, device_map="auto", torch_dtype=torch.float16)
-        # tokenizer = LlamaTokenizer.from_pretrained(model_dir, device_map="auto")
-        # pipe = LlamaForCausalLM.from_pretrained(model_dir, device_map="auto", torch_dtype=torch.float16)
-        # def get_completion(prompt):
-        #     input_ids = tokenizer(prompt, return_tensors="pt").input_ids.to("cuda")
-        #     outputs = pipe.generate(input_ids, max_new_tokens=50, stop=None)
-        #     out = tokenizer.decode(outputs[0], skip_special_tokens=True)
-        #     return out
         for q in queries:
             output = None
-            while output is None or output.isspace() or bool(re.search('[a-zA-Z]', output))==False:
+            while output is None or output.isspace() or bool(re.search('[a-zA-Z]', output)) == False:
                 try:
-                    output = get_completion(q) 
+                    output = get_completion(q)
                     output = output.strip()
                 except Exception as e:
                     print(e)
@@ -151,16 +92,16 @@ def get_response_from_llm(llm_model, queries, task, few_shot, api_num=4):
             if index > 0:
                 output = output[:index]
             print('Model Input: ', q)
-            # print(len(output))
             print('Model Output: ', output)
             model_outputs.append(output)
+
     elif llm_model.lower() == 'chatgpt':
-        # chatgpt
         import openai
+
         def get_completion(prompt):
             response = openai.ChatCompletion.create(
                 model="gpt-3.5-turbo",
-                api_key='', # add your api key
+                api_key='',  # add your api key here
                 messages=[{"role": "user", "content": prompt}],
                 temperature=0.7,
                 top_p=1,
@@ -168,13 +109,14 @@ def get_response_from_llm(llm_model, queries, task, few_shot, api_num=4):
                 presence_penalty=0,
                 stop=None)
             return response["choices"][0]["message"]['content']
+
         for q in queries:
             output = None
             times = 0
             while output is None and times <= 10:
                 try:
-                    times += 1  
-                    output = get_completion(q)   
+                    times += 1
+                    output = get_completion(q)
                 except Exception as e:
                     print(e)
                     print('Retrying...')
@@ -184,12 +126,14 @@ def get_response_from_llm(llm_model, queries, task, few_shot, api_num=4):
                 output = ''
             print('Model Output: ', output)
             model_outputs.append(output)
+
     elif llm_model.lower() == 'gpt4':
         import openai
+
         def get_completion(prompt):
             response = openai.ChatCompletion.create(
-                model="gpt-4", # add your api key
-                api_key="",
+                model="gpt-4",
+                api_key="",  # add your api key here
                 messages=[{"role": "user", "content": prompt}],
                 temperature=0.7,
                 top_p=1,
@@ -197,13 +141,14 @@ def get_response_from_llm(llm_model, queries, task, few_shot, api_num=4):
                 presence_penalty=0,
                 stop=None)
             return response["choices"][0]["message"]['content']
+
         for q in queries:
             output = None
             times = 0
             while output is None and times <= 10:
                 try:
-                    times += 1  
-                    output = get_completion(q)   
+                    times += 1
+                    output = get_completion(q)
                 except Exception as e:
                     print(e)
                     print('Retrying...')
@@ -213,20 +158,20 @@ def get_response_from_llm(llm_model, queries, task, few_shot, api_num=4):
                 output = ''
             print('Model Output: ', output)
             model_outputs.append(output)
+
     elif llm_model.lower() == 'llama2':
+        import torch
         from transformers import LlamaForCausalLM, LlamaTokenizer
 
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+        print(f"Using device: {device}")
+
         tokenizer = LlamaTokenizer.from_pretrained("../Llama-2-13b-chat-hf")
-        model = LlamaForCausalLM.from_pretrained("../Llama-2-13b-chat-hf", device_map="auto")
+        model = LlamaForCausalLM.from_pretrained("../Llama-2-13b-chat-hf").to(device)
+
         for q in queries:
-            # cpu
-            # input_ids = tokenizer(q, return_tensors="pt").input_ids
-            # outputs = model.generate(input_ids)
-            
-            # gpu
-            input_ids = tokenizer(q, return_tensors="pt").input_ids.to("cuda")
+            input_ids = tokenizer(q, return_tensors="pt").input_ids.to(device)
             outputs = model.generate(input_ids, max_new_tokens=50, temperature=0.7, do_sample=True)
-            
             out_text = tokenizer.decode(outputs[0])
             out_text = out_text.replace('<pad>', '')
             out_text = out_text.replace('</s>', '')
@@ -236,55 +181,43 @@ def get_response_from_llm(llm_model, queries, task, few_shot, api_num=4):
             for i in range(len(out_list)):
                 line = out_list[i]
                 if 'Answer:' in line:
-                    cur_line = line.replace('Answer:', '')
-                    cur_line = cur_line.strip()
-                    if  cur_line != '':
+                    cur_line = line.replace('Answer:', '').strip()
+                    if cur_line != '':
                         out_text = cur_line
                         break
                 if 'Output:' in line:
-                    cur_line = line.replace('Output:', '')
-                    cur_line = cur_line.strip()
+                    cur_line = line.replace('Output:', '').strip()
                     if cur_line != '':
                         out_text = cur_line
                         break
                 if 'Answer:' in line:
-                    if i < len(out_list)-1:
-                        next_line = out_list[i+1]
-                        # if 'Input:' not in next_line:
-                        next_line = next_line.strip()
+                    if i < len(out_list) - 1:
+                        next_line = out_list[i+1].strip()
                         if next_line != '':
                             out_text = next_line
                             break
-                        else:
-                            if i < len(out_list)-2:
-                                next_line = out_list[i+2]
-                                # if 'Input:' not in next_line:
-                                next_line = next_line.strip()
-                                if next_line != '':
-                                    out_text = next_line
-                                    break
+                        elif i < len(out_list) - 2:
+                            next_line = out_list[i+2].strip()
+                            if next_line != '':
+                                out_text = next_line
+                                break
                 if 'Output:' in line:
-                    if i < len(out_list)-1:
-                        next_line = out_list[i+1]
-                            # if 'Input:' not in next_line:
-                        next_line = next_line.strip()
+                    if i < len(out_list) - 1:
+                        next_line = out_list[i+1].strip()
                         if next_line != '':
                             out_text = next_line
                             break
-                        else:
-                            if i < len(out_list)-2:
-                                next_line = out_list[i+2]
-                                # if 'Input:' not in next_line:
-                                next_line = next_line.strip()
-                                if next_line != '':
-                                    out_text = next_line
-                                    break
+                        elif i < len(out_list) - 2:
+                            next_line = out_list[i+2].strip()
+                            if next_line != '':
+                                out_text = next_line
+                                break
             out_text = out_text.strip()
             if task == 'cause_and_effect':
                 out_text = 'Sentence ' + out_text
             print('Model Output: ', out_text)
             model_outputs.append(out_text)
-    
+
     else:
         import openai
 
@@ -292,21 +225,21 @@ def get_response_from_llm(llm_model, queries, task, few_shot, api_num=4):
             response = openai.Completion.create(
                 model=llm_model,
                 api_key=API_SET[api_num],
-                prompt=prompt, 
-                temperature=0, 
+                prompt=prompt,
+                temperature=0,
                 max_tokens=1,
-                stop=None, 
-                echo=False, 
-                logprobs=2,)
+                stop=None,
+                echo=False,
+                logprobs=2)
             return response["choices"][0]["text"]
+
         for q in queries:
             output = None
             times = 0
             while output is None and times <= 10:
                 try:
-                    times += 1  
-                    output = get_completion(q)   
-                    # print(output)  
+                    times += 1
+                    output = get_completion(q)
                 except Exception as e:
                     print(e)
                     print('Retrying...')
@@ -316,6 +249,5 @@ def get_response_from_llm(llm_model, queries, task, few_shot, api_num=4):
                 output = ''
             print('Model Output: ', output)
             model_outputs.append(output)
-        
 
     return model_outputs
